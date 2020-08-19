6962cc5e1348a4929bfd9a517a89a9fcd4e03f2c
paszkevi4 <paszkevi4@aol.com>
2020-08-19 14:03:30 +0300

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const audios = document.querySelectorAll("audio");
+
+document.addEventListener('keydown', ((e) => {
+	let curr 
+	audios.forEach(audio => {
+		if (audio.id === e.key) {
+			curr = audio
+		}
+	})
+	curr && curr.play()
+}))
